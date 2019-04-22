399abbbfe6d4e4ce633ce2623ba3593c7b9f74dc
”SKawano” <susumu.kawano@w9.dion.ne.jp>
2019-04-22 00:12:06 +0000

    first commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 15}
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: {with: VALID_EMAIL_REGEX}
+    
+    has_secure_password
+    validates :password, presence: true, length: { minimum: 8, maximum: 32},format: {with: /\d+/}
+    validates :password, presence: true, format: {with: /[a-zA-Z]+/}
+end
