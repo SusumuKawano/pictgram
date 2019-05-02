ec3da73d1e709819bf88d1871e9f70fad262a139
”SKawano” <susumu.kawano@w9.dion.ne.jp>
2019-05-02 04:28:58 +0000

    second commit

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,44 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+#    user = User.find_by(email: params[:session][:email])
+#    if user && user.authenticate(params[:session][:password])
+
+    user = User.find_by(email_params)
+    if user && user.authenticate(password_params[:password])
+      
+      log_in user
+      redirect_to root_path, success: 'ログインに成功しました'
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render :new
+    end
+  end
+  
+  def destroy
+    log_out
+    redirect_to root_url, info: 'ログアウトしました'
+  end
+
+ private
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+  
+  def email_params
+    params.require(:session).permit(:email)
+  end
+
+  def password_params
+    params.require(:session).permit(:password)
+  end
+
+end
+
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
 end
+
+=begin
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+  
+  def logged_in?
+    !current_user.nil?
+  end
+
+=end
